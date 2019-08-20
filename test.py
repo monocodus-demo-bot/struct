bd5f8d74f40d09e55e7138a266a0b3ddd4665686
Pavel Ignatovich <p.ignatovich97@gmail.com>
2019-08-20 14:10:21 +0300

    changed position of AnyOtherClass

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ ade = 6
 fre = 5
 
 
-class TotallyDifferentClass:
-    brea = "a"
-    def __init__(self):
-        pass
-
-
 class AnyOtherMyClass:
     classvar = 6
     def __init__(self, a):
+        pass
+
+
+class TotallyDifferentClass:
+    brea = "a"
+    def __init__(self):
         pass
