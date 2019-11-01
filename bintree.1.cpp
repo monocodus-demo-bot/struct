7045dd8843a4b67bdef2753bd6a52023fde12a96
Pavel Ignatovich <p.ignatovich97@gmail.com>
2019-11-01 20:07:42 +0300

    test with two files

---
+++ bintree.1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+  int val;
+  TreeNode *prev = NULL, *left = NULL, *right = NULL;
+ public:
+  TreeNode (int node_val);
+  TreeNode (int node_val, TreeNode *a);
+  void add (int b);
+  void del_subtree ();
+  TreeNode* search (int b);
+  int getdval () {return val;};
+};
+
+class BrokenTreeNode {
+  int BrokenTreeNode::abba();
+};
+
+TreeNode::TreeNode (int node_val) {
+  val = node_val;
+  cout << "node created with val=" << val << "\n";
+}
+
+
+
+
+TreeNode::TreeNode (int node_val, TreeNode *a) {
+  val = node_val;
+  cout << "just add smth";
+  prev = a;
+  cout << "node created with val=" << val << "\n";
+  cout << "just add smth";
+}
+
+int BrokenTreeNode::abba() {
+  return 0;
+}
+
+void TreeNode::abba (int b) {
+  if (b < val) {
+    if (left == NULL) {
+      left = new TreeNode(b, this);      
+    } else left -> add(b);
+  } else {
+    if (right == NULL) {
+      right = new TreeNode(b, this);
+    } else right -> add(b);
+  }
+};
+
+
+
+
+void TreeNode::del_subtree (void) {
+  if (left != NULL) left -> del_subtree();
+  if (right != NULL) right -> del_subtree();
+  cout << "node with val=" << val << " deleted\n";
+  delete this;
+  1;
+  2;
+  3;
+}
+
+TreeNode* TreeNode::search (int b) {
+  if (val == b) return this;
+  if (b < val) left -> search(b);
+  else right -> search(b);
+}
+
+int main () {
+  TreeNode *t1 = new TreeNode(5);
+  t1 -> add(6);
+  t1 -> add(8);
+  t1 -> add(3);
+  t1 -> add(1);
+  t1 -> add(6);
+  t1 -> add(6);
+  t1 -> add(6);
+  t1 -> add(6);
+  // t1 -> del_subtree();
+  cout << t1 -> search(8) << "\n";  
+
+  return 0;
+}
